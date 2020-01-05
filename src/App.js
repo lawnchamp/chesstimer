4984f905979d9fb1cdb2cfe1170d2ff553ed834b
Riley Shenk <rshenk@rshenk-mac.hq>
2020-01-05 16:57:04 -0500

    Moving all display logic into one component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
-function ClockDisplay({ time }) {
+function ClockDisplay({ isTicking, time, toggleTicker }) {
   const minutes = Math.floor(time / 60);
   const seconds = (time % 60).toString().padStart(2, '0');
 
-  return (
-    <div>
-      {minutes}:{seconds}
-    </div>
-  );
-}
-
-function Clock({ active, toggleTime }) {
-  const [time, setTime] = useState(600);
-
-  useEffect(() => {
-    if (active) {
-      const timeout = setTimeout(() => setTime(time - 1), 1000);
-      return () => clearTimeout(timeout);
-    }
-  });
-
-  const containerStyles = active ? 'shadow-2xl bg-green-900' : 'bg-white';
-  const timerStyles = active ? 'text-gray-100' : 'text-gray-300';
+  const containerStyles = isTicking ? 'shadow-2xl bg-green-900' : 'bg-white';
+  const timerStyles = isTicking ? 'text-gray-100' : 'text-gray-300';
 
   return (
     <div
-      onClick={() => active && toggleTime()}
+      onClick={() => isTicking && toggleTicker()}
       className={`block h-half text-center bg-white ${containerStyles}`}
     >
       <div className={`text-7xl text-gray-300 py-20 md:py-48 ${timerStyles}`}>
-        <ClockDisplay time={time} />
+        <div>
+          {minutes}:{seconds}
+        </div>
       </div>
     </div>
   );
 }
 
+function Clock({ isTicking, toggleTicker }) {
+  const [time, setTime] = useState(600);
+
+  useEffect(() => {
+    if (isTicking) {
+      const timeout = setTimeout(() => setTime(time - 1), 1000);
+      return () => clearTimeout(timeout);
+    }
+  });
+
+  return <ClockDisplay time={time} isTicking={isTicking} toggleTicker={toggleTicker} />;
+}
+
 function App() {
-  const [clockOneIsActive, setClockOneIsActive] = useState(true);
+  const [isTicking, setIsTicking] = useState(true);
 
   return (
     <div className="App h-screen">
-      <Clock active={clockOneIsActive} toggleTime={() => setClockOneIsActive(!clockOneIsActive)} />
-      <Clock active={!clockOneIsActive} toggleTime={() => setClockOneIsActive(!clockOneIsActive)} />
+      <Clock isTicking={isTicking} toggleTicker={() => setIsTicking(!isTicking)} />
+      <Clock isTicking={!isTicking} toggleTicker={() => setIsTicking(!isTicking)} />
     </div>
   );
 }
