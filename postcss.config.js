f6bcc11adee5a5e74e2b4a8a0a1742299bd0196a
Riley Shenk <rjshenk@gmail.com>
2020-01-18 16:14:37 -0500

    wip - purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,21 @@
-const tailwindcss = require('tailwindcss');
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [purgecss]),
+  ],
 };
