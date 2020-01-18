b867e3316baabd5494c18b0420b809b1f4980cb3
Riley Shenk <rjshenk@gmail.com>
2020-01-18 13:25:27 -0500

    finally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-// import { About } from './about';
+import { Link, Route } from 'react-router-dom';
+import { About } from './about';
 // import { Image } from 'cloudinary-react';
 
 function ClockDisplay({ isTicking, time, toggleTicker }) {
@@ -69,7 +69,7 @@ function Tab({ active, children, onClick }) {
     : 'bg-gray-300 text-blue-500 hover:text-blue-800';
   return (
     <li className={`mr-1 ${active ? '-mb-px' : ''}`} onClick={onClick}>
-      <div className={`inline-block rounded-t py-2 px-4 font-semibold ${activeClasses}`} href="#">
+      <div className={`inline-block rounded-t py-2 px-4 font-semibold ${activeClasses}`}>
         {children}
       </div>
     </li>
@@ -81,88 +81,30 @@ function Setup() {
 }
 
 function App() {
+  const [activeTab, setActiveTab] = useState('play');
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/#/about">About</Link>
-            </li>
-            <li>
-              <Link to="/#/users">Users</Link>
-            </li>
+    <div className="App bg-gray-100 px-6 h-screen">
+      <header className="py-6 max-w-xl mx-auto">
+        <nav className="navbar navbar-light">
+          <ul className="flex border-b border-gray-500 mb-2">
+            <Tab onClick={() => setActiveTab('setup')} active={activeTab === 'setup'}>
+              <Link to="/setup">Setup</Link>
+            </Tab>
+            <Tab onClick={() => setActiveTab('play')} active={activeTab === 'play'}>
+              <Link to="/">Play</Link>
+            </Tab>
+            <Tab onClick={() => setActiveTab('about')} active={activeTab === 'about'}>
+              <Link to="/about">About</Link>
+            </Tab>
           </ul>
         </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/#/about">
-            <About />
-          </Route>
-          <Route path="/#/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        <Route path="/about" component={About} />
+        <Route path="/setup" component={Setup} />
+        <Route exact={true} path="/" component={ChessTimer} />
+      </header>
+    </div>
   );
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-// const [activeTab, setActiveTab] = useState('play');
-
-// return (
-//   <div className="App bg-gray-100 px-6 h-screen">
-//     <header className="py-6 max-w-xl mx-auto">
-//       <Router>
-//         <div>
-//           <nav>
-//             <ul className="flex border-b border-gray-500 mb-2">
-//               <Tab onClick={() => setActiveTab('setup')} active={activeTab === 'setup'}>
-//                 <Link to="/setup">Setup</Link>
-//               </Tab>
-//               <Tab onClick={() => setActiveTab('play')} active={activeTab === 'play'}>
-//                 <Link to="/">Play</Link>
-//               </Tab>
-//               <Tab onClick={() => setActiveTab('about')} active={activeTab === 'about'}>
-//                 <Link to="/about">About</Link>
-//               </Tab>
-//             </ul>
-//           </nav>
-
-//           <Switch>
-//             <Route path="/setup">
-//               <Setup />
-//             </Route>
-//             <Route path="/about">
-//               <About />
-//             </Route>
-//             <Route path="/">
-//               <ChessTimer />
-//             </Route>
-//           </Switch>
-//         </div>
-//       </Router>
-//     </header>
-//   </div>
-// );
 
 export default App;
