d844d66695c6a6ad51d7026a7b629175f1000aab
Riley Shenk <rjshenk@gmail.com>
2020-02-21 17:41:39 -0500

    Loading tabs into storybook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Link, Route } from 'react-router-dom';
 import { About } from './about';
 import { ChessTimer } from './chessTimer';
 
-function Tab({ active, children, onClick }) {
+export function Tab({ active, children, onClick }) {
   const activeClasses = active
     ? 'border-gray-500 bg-gray-100 border-l border-t border-r text-blue-800'
     : 'text-blue-500 hover:text-blue-800';
+
   return (
     <li className={`mr-1 ${active ? '-mb-px' : ''}`} onClick={onClick}>
       <div className={`inline-block rounded-t py-2 px-4 font-medium ${activeClasses}`}>
@@ -16,38 +17,46 @@ function Tab({ active, children, onClick }) {
   );
 }
 
+function Tabs() {
+  const [activeTab, setActiveTab] = useState(window.location.hash);
+
+  return (
+    <Fragment>
+      <ul className="flex border-b border-gray-500 mb-2">
+        <Link to="/setup">
+          <Tab onClick={() => setActiveTab('#/setup')} active={activeTab === '#/setup'}>
+            Setup
+          </Tab>
+        </Link>
+        <Link to="/">
+          <Tab onClick={() => setActiveTab('#/')} active={activeTab === '#/'}>
+            Play
+          </Tab>
+        </Link>
+        <Link to="/about">
+          <Tab onClick={() => setActiveTab('#/about')} active={activeTab === '#/about'}>
+            About
+          </Tab>
+        </Link>
+      </ul>
+      <Route path="/setup" component={Setup} />
+      <Route exact={true} path="/" component={ChessTimer} />
+      <Route path="/about" component={About} />
+    </Fragment>
+  );
+}
+
 function Setup() {
   return <h2 className="my-2 font-semibold text-2xl text-gray-700">Coming soon!</h2>;
 }
 
 function App() {
-  const [activeTab, setActiveTab] = useState(window.location.hash);
-
   return (
     <div className="App bg-gray-100 px-6 h-screen">
       <header className="py-6 max-w-xl mx-auto">
         <nav className="navbar navbar-light">
-          <ul className="flex border-b border-gray-500 mb-2">
-            <Link to="/setup">
-              <Tab onClick={() => setActiveTab('#/setup')} active={activeTab === '#/setup'}>
-                Setup
-              </Tab>
-            </Link>
-            <Link to="/">
-              <Tab onClick={() => setActiveTab('#/')} active={activeTab === '#/'}>
-                Play
-              </Tab>
-            </Link>
-            <Link to="/about">
-              <Tab onClick={() => setActiveTab('#/about')} active={activeTab === '#/about'}>
-                About
-              </Tab>
-            </Link>
-          </ul>
+          <Tabs />
         </nav>
-        <Route path="/setup" component={Setup} />
-        <Route exact={true} path="/" component={ChessTimer} />
-        <Route path="/about" component={About} />
       </header>
     </div>
   );
---
+++ src/stories/4-tabs.stories.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Tab } from '../App';
+import { action } from '@storybook/addon-actions';
+import '../styles/tailwind.css';
+import { useState } from 'react';
+
+export default {
+  title: 'Tabs',
+  component: 'Tab',
+};
+
+export const HardCodedTabs = () => {
+  return (
+    <ul className="flex border-b border-gray-500 mb-2">
+      <Tab onClick={action('tab 1 clicked')} active={false}>
+        Setup
+      </Tab>
+
+      <Tab onClick={action('tab 2 clicked')} active={true}>
+        Play
+      </Tab>
+
+      <Tab onClick={action('tab 3 clicked')} active={false}>
+        About
+      </Tab>
+
+      <Tab onClick={action('tab 3 clicked')} active={false}>
+        Contact Us
+      </Tab>
+
+      <Tab onClick={action('tab 3 clicked')} active={false}>
+        Pricing
+      </Tab>
+    </ul>
+  );
+};
+
+export const ReactiveTabs = () => {
+  const [activeTab, setActiveTab] = useState(2);
+
+  return (
+    <ul className="flex border-b border-gray-500 mb-2">
+      <Tab onClick={() => setActiveTab(1)} active={activeTab === 1}>
+        Setup
+      </Tab>
+
+      <Tab onClick={() => setActiveTab(2)} active={activeTab === 2}>
+        Play
+      </Tab>
+
+      <Tab onClick={() => setActiveTab(3)} active={activeTab === 3}>
+        About
+      </Tab>
+
+      <Tab onClick={() => setActiveTab(4)} active={activeTab === 4}>
+        Contact Us
+      </Tab>
+
+      <Tab onClick={() => setActiveTab(5)} active={activeTab === 5}>
+        Pricing
+      </Tab>
+    </ul>
+  );
+};
+
+export const PureTabs = () => {
+  return (
+    <div>
+      <Tab onClick={action('tab 1 clicked')} active={false}>
+        Setup
+      </Tab>
+      <Tab onClick={action('tab 2 clicked')} active={true}>
+        play
+      </Tab>
+      <Tab onClick={action('tab 3 clicked')} active={false}>
+        fun
+      </Tab>
+    </div>
+  );
+};
