4195726ba41bcf5d299e81c9217be16df5a20b6b
Riley Shenk <rjshenk@gmail.com>
2020-03-05 22:44:50 -0500

    Removed rotation logic for simplification

--- src/chessTimer.js
+++ src/chessTimer.js
@@ -1,46 +1,24 @@
 import React, { useState, Fragment, useEffect } from 'react';
 
 export function ClockDisplay({ isTicking, time, toggleTicker }) {
-  const [rotation, setRotation] = useState(0);
-
-  function onRotationClicked(event) {
-    event.stopPropagation();
-    setRotation(rotation + 45);
-  }
-
   const minutes = Math.floor(time / 60);
   const seconds = (time % 60).toString().padStart(2, '0');
 
   const containerStyles = isTicking
     ? 'bg-gray-300 border border-gray-700 shadow-2xl'
     : 'bg-white border';
-  const timerStyles = isTicking
-    ? 'text-gray-700'
-    : 'text-gray-300 border border-white';
+  const timerStyles = isTicking ? 'text-gray-700' : 'text-gray-300';
 
   return (
-    <div className="relative">
-      <button
-        disabled={!isTicking || time <= 0}
-        onClick={toggleTicker}
-        className={`${containerStyles} w-full rounded-lg h-half focus:outline-none`}
-      >
-        <svg
-          className={`${timerStyles} absolute top-0 right-0 w-8 h-8 m-2 fill-current hover:text-gray-900`}
-          onClick={onRotationClicked}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M15 17v-2.99A4 4 0 0 0 11 10H8v5L2 9l6-6v5h3a6 6 0 0 1 6 6v3h-2z" />
-        </svg>
-        <div
-          className={`${timerStyles} text-8xl`}
-          style={{ transform: `rotate(${rotation}deg)` }}
-        >
-          {minutes}:{seconds}
-        </div>
-      </button>
-    </div>
+    <button
+      disabled={!isTicking || time <= 0}
+      onClick={toggleTicker}
+      className={`${containerStyles} relative w-full rounded-lg h-half focus:outline-none`}
+    >
+      <div className={`${timerStyles} text-8xl`}>
+        {minutes}:{seconds}
+      </div>
+    </button>
   );
 }
 
